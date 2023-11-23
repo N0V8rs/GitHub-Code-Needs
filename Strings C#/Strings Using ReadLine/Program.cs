44ded7f6e58383b5fa8563b360e6f86b5fb9c654
N0V8rs <144738466+N0V8rs@users.noreply.github.com>
2023-11-23 03:41:55 -0400

    Added Strings Readline
    Using strings as a readline

---
+++ Strings C#/Strings Using ReadLine/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_using_ReadLine
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your name (Press Esc to exit):");
+
+            while (true)
+            {
+                // Read a key from the console
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                // Check if the key pressed is the Escape key
+                if (keyInfo.Key == ConsoleKey.Escape)
+                    break;
+
+                // If it's not the Escape key, continue reading the rest of the name
+                string userName = keyInfo.KeyChar + Console.ReadLine();
+
+                // Greet the user
+                Console.WriteLine("Hello, " + userName + "! Nice to meet you.");
+            }
+        }
+    }
+}
